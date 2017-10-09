211842734b910597d1e25047fab05478a68b987c
simonaco-circle <simona_cotin@yahoo.com>
2017-10-09 12:03:20 +0100

    Don't push to new branch

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
-git config --global user.email simona.cotin@microsoft.com
+git config --global user.email simona.cotin@yahoo.com
 git config --global user.name simonaco-circle
-
-git checkout -b "$1"
-git merge master
-git commit -am "$CIRCLE_BRANCH build#$CIRCLE_BUILD_NUM"
-git push --force --set-upstream origin $1
-
 
 # $AZURE_REPO_URL needs to be set in your projects Variables section
 # and include both username and password, e.g: https://username:password@site.scm.azurewebsites.net:443/site.git
